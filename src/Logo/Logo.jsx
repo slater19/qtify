e9030ebffc5b73b5ead49aad5495e79531bce010
slater19 <sachinaggarwal19@gmail.com>
2024-09-14 18:23:12 +0530

    first3

--- src/Logo/Logo.jsx
+++ src/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoImage from "../assets/logo.svg";
+import styles from "../Navbar/Navbar.module.css";
 
 export default function Logo() {
-   return <img src={LogoImage} alt="logo" width={67} />;
+   return <div className={styles.logoDiv}><img src={LogoImage} alt="logo" width={67} /></div>;
 } 
